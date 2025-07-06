662121a43cf1223b3161cc382a45d89243260178
helton1985 <helton.btc@gmail.com>
2025-07-06 19:19:11 -0300

    Create auth.js

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+module.exports = async (req, res, next) => {
+  try {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+    if (!token) {
+      return res.status(401).json({ message: 'Acesso negado. Nenhum token fornecido.' });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById(decoded.id).select('-password');
+    if (!req.user) {
+      return res.status(401).json({ message: 'Usuário não encontrado.' });
+    }
+    next();
+  } catch (error) {
+    res.status(400).json({ message: 'Token inválido.' });
+  }
+};
